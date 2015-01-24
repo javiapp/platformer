a09d3996248c29a1f5576c10c4dd3844691c8731
Javi <jsoliz@pasedfoundation.org>
2015-01-23 18:12:02 -0800

    [37-38] Added Solid Ground shapes in Tiled
    Read code from level map and create box2d bodies from that information

--- core/src/com/solizj/platformer/model/Level.java
+++ core/src/com/solizj/platformer/model/Level.java
@@ -1,5 +1,6 @@
 package com.solizj.platformer.model;
 
+import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 
@@ -8,6 +9,12 @@ public class Level {
 
     public Level(String mapPath){
         map = new TmxMapLoader().load(mapPath);
+    }
 
+    public MapLayer getMapLayer(String layerName){ // [38] Gt the layer found in the map
+        return map.getLayers().get(layerName);
+    }
+    public MapObjects getLayerObjects(MapLayer mapLayer){// [38] Get objects found within the layer
+        return mapLayer.getObjects();
     }
 }
